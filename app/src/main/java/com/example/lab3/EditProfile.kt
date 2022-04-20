f69f98fdb25666dcbc56b0f1796f89a0260c0604
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-04-20 20:29:32 +0200

    Added camera intent

--- app/src/main/java/com/example/lab3/EditProfile.kt
+++ app/src/main/java/com/example/lab3/EditProfile.kt
@@ -1,10 +1,13 @@
 package com.example.lab3
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.edit_profile_fragment.view.*
@@ -35,6 +38,14 @@ class EditProfile : Fragment() {
             userViewModel.location.value = view.locationET.text.toString()
             userViewModel.username.value = view.usernameET.text.toString()
             findNavController().popBackStack()
+        }
+
+        view.editProfileImageButton.setOnClickListener {
+            val galleryIntent = Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT)
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val chooserIntent = Intent.createChooser(galleryIntent, "Upload an Image").run { putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(cameraIntent)) }
+
+            activity?.startActivityForResult(chooserIntent, (activity as MainActivity).requestPhotoForProfileEdit)
         }
 
         return view
--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.lab3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
 
     val userViewModel: UserViewModel by viewModels()
+    val requestPhotoForProfileEdit = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == requestPhotoForProfileEdit) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    Toast.makeText(this, "Photo recieved correctly", Toast.LENGTH_SHORT).show()
+                }
+                RESULT_CANCELED -> {
+                    Toast.makeText(this, "User cancelled", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }

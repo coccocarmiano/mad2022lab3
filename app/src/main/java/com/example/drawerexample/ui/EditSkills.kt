5dc55a84647d23ac6e5211397f250239d5c74da5
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-05-12 15:54:54 +0200

    Partial implementation of skill management

--- app/src/main/java/com/example/drawerexample/ui/EditSkills.kt
+++ app/src/main/java/com/example/drawerexample/ui/EditSkills.kt
@@ -4,47 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drawerexample.adapter.SkillsAdapter
 import com.example.drawerexample.databinding.EditSkillsFragmentBinding
 import com.example.drawerexample.viewmodel.UserViewModel
+import com.google.firebase.firestore.FirebaseFirestore
 
 class EditSkills : Fragment() {
 
     private val userViewModel: UserViewModel by activityViewModels()
-
-    private var _binding: EditSkillsFragmentBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding : EditSkillsFragmentBinding
+    private var skillsList : MutableLiveData<List<String>> = MutableLiveData()
+    private lateinit var skillsAdapter : SkillsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = EditSkillsFragmentBinding.inflate(inflater, container, false)
+        binding = EditSkillsFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val skillsAdapter = SkillsAdapter(
-            listOf(
-                "Gardening", "App Development", "Cooking", "Programming", "Reading",
-            ).toTypedArray(), userViewModel.liveSkills)
-        val recyclerView = binding.skillsListRecyclerView
-        recyclerView.run {
-            adapter = skillsAdapter
-            layoutManager = LinearLayoutManager(container?.context)
+        skillsList.observe(viewLifecycleOwner) {
+            skillsAdapter = SkillsAdapter(it.toTypedArray(), userViewModel.liveSkills)
+            binding.skillsListRecyclerView.apply {
+                adapter = skillsAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
         }
 
+        getSkillsList()
+
         binding.saveSkillButton.setOnClickListener {
-            activity?.onBackPressed()
+            skillsList.value?.run {
+                userViewModel.updateSkills(this)
+            }
+            findNavController().popBackStack()
+        }
+
+        requireActivity()
+            .onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().popBackStack()
         }
 
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun getSkillsList() {
+        FirebaseFirestore
+            .getInstance()
+            .collection("general")
+            .document("skills")
+            .get()
+            .addOnSuccessListener {
+                skillsList.value = try {
+                    it.data?.get("skills") as List<String>
+                } catch (e: ClassCastException) {
+                    e.printStackTrace()
+                    listOf()
+                }
+            }
+            .addOnFailureListener {
+                skillsList = MutableLiveData()
+            }
     }
 }

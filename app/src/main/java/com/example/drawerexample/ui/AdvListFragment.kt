f67a0f0f3974fc223f818ede6eb147d186529272
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-05-16 22:35:34 +0200

    Changed delegate instantiation to the correct one

--- app/src/main/java/com/example/drawerexample/ui/AdvListFragment.kt
+++ app/src/main/java/com/example/drawerexample/ui/AdvListFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drawerexample.R
@@ -15,7 +15,7 @@ import com.example.drawerexample.databinding.FragmentAdvListBinding
 
 class AdvListFragment : Fragment() {
 
-    private val advViewModel: AdvertisementViewModel by activityViewModels()
+    private val advViewModel: AdvertisementViewModel by viewModels()
     private lateinit var binding : FragmentAdvListBinding
 
     override fun onCreateView(

5bd7ca1157947a00f2ddd0b20267b7d20da5f034
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-05-17 19:55:40 +0200

    Added methods to upload and download images from firebase

--- app/src/main/java/com/example/drawerexample/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/drawerexample/viewmodel/UserViewModel.kt
@@ -2,6 +2,8 @@ package com.example.drawerexample.viewmodel
 
 import android.app.Application
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
@@ -12,6 +14,10 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import java.nio.ByteBuffer
 
 class UserViewModel(val app : Application) : AndroidViewModel(app) {
 
@@ -30,7 +36,9 @@ class UserViewModel(val app : Application) : AndroidViewModel(app) {
             }.addOnFailureListener {
                 Toast.makeText(app, "Failed to get user document", Toast.LENGTH_LONG).show()
             }
+
         attachUserInfoListener()
+        setUserPhoto()
     }
 
 
@@ -41,12 +49,14 @@ class UserViewModel(val app : Application) : AndroidViewModel(app) {
             "username" to app.getString(R.string.username_placeholder_text),
             "email" to ( currentUser?.email ?: app.getString(R.string.email_placeholder_text) ),
             "location" to app.getString(R.string.location_placeholder_text),
-            "skills" to listOf<String>()
+            "skills" to listOf<String>(),
+            "pfpProg" to 0,
         )
 
         userDocumentReference.set(userHashMap).addOnFailureListener {
             Log.e("UserViewModel", "Failed to create default user document")
         }
+
     }
 
 
@@ -84,7 +94,7 @@ class UserViewModel(val app : Application) : AndroidViewModel(app) {
                 "email" to mail,
                 "location" to location,
                 "skills" to skills,
-                "username" to username
+                "username" to username,
             )
             userDocumentReference.set(userHashMap)
         }
@@ -112,6 +122,71 @@ class UserViewModel(val app : Application) : AndroidViewModel(app) {
                 }
             }
         }
+    }
+
+    private fun setUserPhoto() {
+        setPhotoFromFirebase()
+    }
+
+    private fun setPhotoFromGoogle() {
+        when (val photoURI = Firebase.auth.currentUser?.photoUrl) {
+            null -> {
+                setDefaultPhoto()
+            }
+            else -> {
+                setProfilePictureFromURI(photoURI)
+            }
+        }
+    }
+
+    private fun setPhotoFromFirebase() {
+        Firebase.storage.reference
+            .child("users_profile_pictures")
+            .child(uid)
+            .downloadUrl
+            .addOnSuccessListener {
+                setProfilePictureFromURI(it)
+            }.addOnFailureListener {
+                setPhotoFromGoogle()
+            }
+    }
+
+    private fun setProfilePictureFromURI(photoURI : Uri) {
+        Thread {
+            URL(photoURI.toString()).openStream().use {
+                val bitmap = BitmapFactory.decodeStream(it)
+                livePicture.postValue(bitmap)
+                uploadPhotoToFirebase(bitmap)
+            }
+        }.start()
+    }
+
+    private fun setDefaultPhoto() {
+        val bmp = BitmapFactory.decodeResource(app.resources, R.drawable.default_pfp)
+        livePicture.value = bmp
+        uploadPhotoToFirebase(bmp)
+    }
+
+    private fun uploadPhotoToFirebase(bmp : Bitmap) {
+        val byteOutStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, byteOutStream)
+        val byteArray = byteOutStream.toByteArray()
+        Firebase.storage.reference
+            .child("users_profile_pictures")
+            .child(uid)
+            .putBytes(byteArray)
+            .addOnFailureListener {
+                Toast.makeText(app, "Failed to upload photo: ${it.toString()}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun updateProfilePictureFromBitmap(bmp : Bitmap) {
+        livePicture.postValue(bmp)
+        uploadPhotoToFirebase(bmp)
+    }
+
+    fun updateProfilePictureFromURI(uri : Uri) {
+        setProfilePictureFromURI(uri)
     }
 }
 

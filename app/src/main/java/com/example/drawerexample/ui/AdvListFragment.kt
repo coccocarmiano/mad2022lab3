4ab6c9190f86725cbf2dc568b8f0f7fd1883aab6
RobertoTorta <s290184@studenti.polito.it>
2022-05-19 14:46:35 +0200

    partial filter added

--- app/src/main/java/com/example/drawerexample/ui/AdvListFragment.kt
+++ app/src/main/java/com/example/drawerexample/ui/AdvListFragment.kt
@@ -5,23 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.drawerexample.Advertisement
 import com.example.drawerexample.R
 import com.example.drawerexample.adapter.AdvertisementsAdapter
 import com.example.drawerexample.viewmodel.AdvertisementViewModel
 import com.example.drawerexample.databinding.FragmentAdvListBinding
+import com.example.drawerexample.viewmodel.UserViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.firestore.ktx.toObjects
 
 class AdvListFragment : Fragment() {
 
     private val advViewModel: AdvertisementViewModel by viewModels()
+    private val userViewModel: UserViewModel by activityViewModels()
     private lateinit var binding : FragmentAdvListBinding
+    private lateinit var skillsSpinnerAdapter: ArrayAdapter<String>
+    private val skillsMap = HashMap<String, Boolean>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
 
         binding = FragmentAdvListBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -34,9 +46,47 @@ class AdvListFragment : Fragment() {
             layoutManager = LinearLayoutManager(container?.context)
         }
 
+        //advViewModel.liveAdvList.observe(viewLifecycleOwner) {
+        //    advAdapter.data = it
+        //    advAdapter.notifyDataSetChanged()
+        //}
+
+
+        skillsSpinnerAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, mutableListOf())
+        skillsSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.skillSpinner.adapter = skillsSpinnerAdapter
+        skillsSpinnerAdapter.clear()
+        skillsSpinnerAdapter.add("All")
+        skillsSpinnerAdapter.addAll(skillsMap.keys)
+        binding.skillSpinner.setSelection(0, true)
+        skillsMap.clear()
+        FirebaseFirestore
+            .getInstance()
+            .collection("general")
+            .get()
+            .addOnSuccessListener { docs ->
+                docs.forEach { doc ->
+                    doc.get("skills")
+                        ?.let { it as List<String> }
+                        ?.onEach { skillsMap[it] = userViewModel.liveUser.value?.skills?.contains(it) ?: true }
+                        ?.also {
+                            skillsSpinnerAdapter.addAll(it)
+                            skillsSpinnerAdapter.notifyDataSetChanged()
+                        }
+                }
+            }
+
+
+
         advViewModel.liveAdvList.observe(viewLifecycleOwner) {
-            advAdapter.data = it
+            val myAdvertisementList : List<Advertisement>
+            if(binding.skillSpinner.selectedItem=="All")
+                myAdvertisementList =it.filter { true }
+            else
+                myAdvertisementList =it.filter { it.skill==binding.skillSpinner.selectedItem }
+            advAdapter.data = myAdvertisementList.toMutableList()
             advAdapter.notifyDataSetChanged()
+
         }
 
         binding.advAddFb.setOnClickListener {
@@ -48,5 +98,5 @@ class AdvListFragment : Fragment() {
         }
 
         return root
-    }
-}
+    } }
+
--- app/src/main/java/com/example/drawerexample/ui/MyAdvListFragment.kt
+++ app/src/main/java/com/example/drawerexample/ui/MyAdvListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
@@ -23,6 +24,10 @@ class MyAdvListFragment : Fragment() {
     private val userViewModel: UserViewModel by activityViewModels()
     private lateinit var binding : FragmentAdvListBinding
 
+    private lateinit var skillsSpinnerAdapter: ArrayAdapter<String>
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,10 +45,27 @@ class MyAdvListFragment : Fragment() {
             layoutManager = LinearLayoutManager(container?.context)
         }
 
+        skillsSpinnerAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, mutableListOf())
+        skillsSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.skillSpinner.adapter = skillsSpinnerAdapter
+
+        userViewModel.liveUser.observe(viewLifecycleOwner) {
+            skillsSpinnerAdapter.clear()
+            skillsSpinnerAdapter.add("All")
+            skillsSpinnerAdapter.addAll(it.skills)
+            binding.skillSpinner.setSelection(0, true)
+        }
+
+
         advViewModel.liveAdvList.observe(viewLifecycleOwner) {
-            val myAdvertisementList =it.filter { it.emailCreator == userViewModel.liveUser.value!!.mail }
+            val myAdvertisementList : List<Advertisement>
+            if(binding.skillSpinner.selectedItem=="All")
+                myAdvertisementList =it.filter { it.emailCreator == userViewModel.liveUser.value!!.mail  }
+            else
+                myAdvertisementList =it.filter { it.emailCreator == userViewModel.liveUser.value!!.mail && it.skill==binding.skillSpinner.selectedItem }
             advAdapter.data = myAdvertisementList.toMutableList()
             advAdapter.notifyDataSetChanged()
+
         }
 
         binding.advAddFb.setOnClickListener {

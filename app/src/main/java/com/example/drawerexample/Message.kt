644804a8ff2f497b660f3c1f877a9b69269fafa0
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-05-29 21:25:30 +0200

    new: ChatViewModel class and Message dataclass

---
+++ app/src/main/java/com/example/drawerexample/Message.kt
@@ -0,0 +1,4 @@
+package com.example.drawerexample
+
+
+data class Message(val text: String, val timestamp : Long, val sender : String, val receiver : String)
---
+++ app/src/main/java/com/example/drawerexample/viewmodel/ChatViewModel.kt
@@ -0,0 +1,86 @@
+package com.example.drawerexample.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.drawerexample.Message
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class ChatViewModel : ViewModel() {
+    var messages : MutableLiveData<ArrayList<Message>> = MutableLiveData()
+    private var advertisementID : MutableLiveData<String> = MutableLiveData()
+    private var advertiserID : String? = null
+    private var otherUserID : String? = null
+    private val db = Firebase.firestore
+
+    init {
+        advertisementID.observeForever {
+            updateAdvertiserID()
+            updateListener()
+        }
+    }
+
+    fun applyAdvertisementID(id : String){
+        advertisementID.value = id
+    }
+
+    fun setRequesterID(id : String){
+        advertiserID = id
+    }
+
+    private fun updateAdvertiserID() {
+        db.collection("advertisements").document("${advertisementID.value}").get().addOnSuccessListener {
+            advertiserID = it.getString("creatorID")
+        }
+    }
+
+    private fun updateListener() {
+        db
+            .collection("chats")
+            .document("${advertisementID.value}")
+            .collection("$otherUserID")
+            .orderBy("timestamp", Query.Direction.DESCENDING)
+            .addSnapshotListener { doc, err ->
+                when ( err == null) {
+                    true -> updateCollection(doc)
+                    else -> Log.w("ChatViewModel", "Error getting documents.", err)
+                }
+            }
+    }
+
+    private fun updateCollection(doc: QuerySnapshot?) {
+        doc?.documents?.map {
+            val text = it.getString("text") ?: ""
+            val sender = it.getString("sender") ?: ""
+            val receiver = it.getString("receiver") ?: ""
+            val timestamp = it.getLong("timestamp") ?: 0L
+            Message(text, timestamp, sender, receiver)
+        }?.toCollection(ArrayList())
+            .let {
+                messages.value = it
+            }
+    }
+
+    private fun postMessage(text: String, sender : String) {
+        val receiver = when (sender) {
+            advertiserID -> otherUserID
+            else -> advertiserID
+        }
+
+        db
+            .collection("chats")
+            .document("${advertisementID.value}")
+            .collection("$otherUserID")
+            .add(
+                hashMapOf(
+                    "text" to text,
+                    "sender" to sender,
+                    "receiver" to receiver,
+                    "timestamp" to System.currentTimeMillis()
+                )
+            )
+    }
+}
